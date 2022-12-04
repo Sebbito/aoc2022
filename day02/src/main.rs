633d39e559030c41fc52bad03392e9810cbe96d1
Sebbito <eric.bortz124@gmail.com>
2022-12-04 16:49:16 +0100

    improved answer to part 1 and added solution to part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,14 +1,6 @@
 use std::collections::HashMap;
 use std::fs;
 
-// A/X = Rock     = 1 P
-// B/Y = Paper    = 2 P
-// C/Z = Scissors = 3 P
-//
-// lost = 0
-// draw = 3
-// won  = 6
-
 // File path since i'm lazy...again
 const PATH_TO_FILE: &str = "input";
 
@@ -16,9 +8,7 @@ fn assert_win(opponent_hand: i32, my_hand: i32) -> i32{
     if opponent_hand.eq(&my_hand){
         // draw
         3 + my_hand
-    } else if (opponent_hand == (1) && my_hand == (2)) || // rock, paper
-              (opponent_hand == (2) && my_hand == (3)) || // paper, scissors
-              (opponent_hand == (3) && my_hand == (1)) {  // scissors, rock
+    } else if ((opponent_hand % 3) + 1).eq(&my_hand) {
         // win
         6 + my_hand
     } else {
@@ -27,29 +17,64 @@ fn assert_win(opponent_hand: i32, my_hand: i32) -> i32{
     }
 }
 
+fn find_hand(opponent_hand: i32, result: i32) -> i32 {
+    let mut my_hand = 0;
+
+    if result == 3 { // draw
+        my_hand = opponent_hand;
+    } else if result == 6 { // win
+        my_hand = (opponent_hand % 3) + 1;
+    } else { // lose
+        my_hand = opponent_hand + 2;
+        if my_hand.gt(&3){
+            my_hand %= 3;
+        }
+    }
+    result + my_hand
+}
+
 fn main() {
     let mut score = HashMap::new();
 
-    score.insert("A", 1);
-    score.insert("B", 2);
-    score.insert("C", 3);
-    score.insert("X", 1);
-    score.insert("Y", 2);
-    score.insert("Z", 3);
+    score.insert("A", 1); // Rock
+    score.insert("B", 2); // Paper
+    score.insert("C", 3); // Scissors
+    score.insert("X", 1); // Rock
+    score.insert("Y", 2); // Paper
+    score.insert("Z", 3); // Scissors
+
+    let mut win_value = HashMap::new();
+
+    win_value.insert("X", 0); // lose
+    win_value.insert("Y", 3); // draw
+    win_value.insert("Z", 6); // win
     
     // Read file
     let file_contents= fs::read_to_string(PATH_TO_FILE).expect("Could not read file");
 
-    let mut scores: Vec<i32> = Vec::new();
+    let mut scores_p1: Vec<i32> = Vec::new();
+    let mut scores_p2: Vec<i32> = Vec::new();
     
+    // doing both parts in one loop i don't care
     for line in file_contents.lines() {
         let mut hands = line.split_whitespace();
         let opponent = score.get(hands.next().unwrap()).unwrap();
-        let my       = score.get(hands.next().unwrap()).unwrap();
-        scores.push(assert_win(*opponent, *my));
+
+        let v2 = hands.next().unwrap();
+        let my_hand = score.get(v2).unwrap(); // part 1
+        let result =  win_value.get(v2).unwrap(); // part 2
+
+        scores_p1.push(assert_win(*opponent, *my_hand));
+        scores_p2.push(find_hand(*opponent, *result));
     }
 
-    let total: i32= scores.iter().sum();
-    println!("Total score: {}", total);
+    // part one
+    let total: i32= scores_p1.iter().sum();
+    println!("Part 1 Total score: {}", total);
+
+    // part two
+    let total: i32 = scores_p2.iter().sum();
+    println!("Part 2 Total Score: {}", total);
+
 
 }
