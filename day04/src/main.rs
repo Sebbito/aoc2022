a8e86aee6840e662a7a25aaea0b431478e6faf27
Sebbito <eric.bortz124@gmail.com>
2022-12-07 14:31:31 +0100

    added solution to part 1 and part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -4,32 +4,33 @@ const PATH_TO_FILE: &str = "input";
 
 fn to_range(range: String) -> Range<i32> {
     let min = range.split_at(range.find("-").unwrap()).0;
-    let max = range.split_at(range.find("-").unwrap()).1;
+    let max = range.split_at(range.find("-").unwrap() + 1).1;
     Range { start: i32::from_str_radix(min, 10).unwrap(), end: i32::from_str_radix(max, 10).unwrap()}
 }
 
 fn read_line(line: String) -> (Range<i32>, Range<i32>){
     let comma_pos = line.find(',').unwrap(); // split at ','
-    let mut iterator = line.chars();
+    let iterator = line.chars();
     // transform string with form '1-4' to range with start 1 and end 4
     let r1 = to_range(iterator.clone().take(comma_pos).collect());
-    iterator.next(); // skip once so that comma will be skipped
-    let r2 = to_range(iterator.skip(comma_pos).take(line.len()-comma_pos+1).collect());
+    let r2 = to_range(iterator.skip(comma_pos+1).take(line.len()-comma_pos).collect());
     (r1, r2)
 }
 
 
 fn is_containted(r1: Range<i32>, r2: Range<i32>) -> bool {
     // check if one range is contained in another
-    // 
-    // range 1 contains range 2 example: 1-4,4-4
-    if  (r1.start <= r2.start) && (r1.end >= r2.end) ||
-        (r2.start <= r1.start) && (r2.end >= r1.end) {
-        true
-    } else {
-        false
-    }
+
+    // range 1 contains range 2 or vice versa
+    (r1.start <= r2.start) && (r1.end >= r2.end) ||
+    (r2.start <= r1.start) && (r2.end >= r1.end)
 }
+
+fn does_overlap(r1: Range<i32>, r2: Range<i32>) -> bool {
+    // range 1 and range 2 overlap at all
+    !(r1.start > r2.end || r2.start > r1.end)
+}
+
 
 fn main() {
     let file: String = fs::read_to_string(PATH_TO_FILE).expect("File not found");
@@ -40,7 +41,9 @@ fn main() {
         range_vec.push((r1, r2));
     }
 
-    let sum = range_vec.iter().map(|(r1, r2)| is_containted(r1.clone(), r2.clone())).filter(|&x| x == true).count();
+    let part1 = range_vec.iter().map(|(r1, r2)| is_containted(r1.clone(), r2.clone())).filter(|&x| x == true).count();
+    let part2 = range_vec.iter().map(|(r1, r2)| does_overlap(r1.clone(), r2.clone())).filter(|&x| x == true).count();
 
-    println!("{}", sum);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
